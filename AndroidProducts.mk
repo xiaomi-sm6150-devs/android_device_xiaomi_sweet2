cb356247a473a0a839536e37cdedeed8f6d5536f
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 14:04:59 +0000

    sweet: Redmi Note 10 Pro -> Redmi Note 12 Pro
    Change-Id: I1ea3fd8559ff9388abdf250526a6f786c1ed2d8f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021, 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/lineage_sweet2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    lineage_sweet2-user \
+    lineage_sweet2-userdebug \
+    lineage_sweet2-eng
--- extract-files.sh
+++ extract-files.sh
@@ -22,7 +22,7 @@ fi
 
 set -e
 
-export DEVICE=sweet
+export DEVICE=sweet2
 export DEVICE_COMMON=sm6150-common
 export VENDOR=xiaomi
 
